3f8999f47e92f1e02d91488b28e688c11afbdc1e
Jerry Howard <jerryhoward@fastmail.fm>
2019-08-05 21:42:49 +0000

    Adds some seed data for the models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+row_count = 5
+
+row_count.times do |num|
+  Bookmark.create!(title: "Seeded bookmark #{num}", 
+                     url: "https://example.com/test/#{num}", 
+                     desc: "This bookmark was seeded by Rails")
+end
+puts "#{row_count} bogus bookmarks created"
+
+row_count.times do |num|
+  User.create!(name: "Seeded user #{num}", email: "seeded_user_#{num}@example.com")
+end
+puts "#{row_count} bogus users created"
+
+row_count.times do |num|
+  Group.create!(title: "Seeded group #{num}")
+end
+puts "#{row_count} bogus groups created"

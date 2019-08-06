380b2680f9a215e116c044b7dee5419ec8266999
Jerry Howard <jerryhoward@fastmail.fm>
2019-08-06 22:28:01 +0000

    Adds support for a profile image on the User model, as well as a default placeholder image generator exposed as a model concern

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
-    has_and_belongs_to_many :groups
-    
-    validates_presence_of :name, :email
+  has_and_belongs_to_many :groups
+
+  validates_presence_of :name, :email
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.profile_image ||= Placeholder.image_generator(width: '150', height: '150')
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_212653) do
+ActiveRecord::Schema.define(version: 2019_08_06_221857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2019_08_06_212653) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "profile_image"
   end
 
   add_foreign_key "bookmarks", "groups"

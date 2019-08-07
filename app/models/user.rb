00c88c1010a1819ad96c0db53a71bfd9ab7ad1b4
Jerry Howard <jerryhoward@fastmail.fm>
2019-08-07 21:41:21 +0000

    Adds :trackable module for Devise + migration lines to enable it. Also skips 'email' field in migration as we already have that.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable
   has_and_belongs_to_many :groups
 
   validates_presence_of :name, :email

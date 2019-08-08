7cd87a4e5fdac222583748eb8c10ce987c773feb
Jerry Howard <jerryhoward@fastmail.fm>
2019-08-08 20:27:06 +0000

    Simple homepage demo to verify that login/logout/register, user vs admin designation, and sessions are working as expected

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def login_helper(style = "")
+    if current_user # Is there a user? Will be nil otherwise.
+      @is_user = current_user.is_a?(User)
+      @is_admin = current_user.admin?
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
+    else
+      (link_to "Login", new_user_session_path, class: style) + 
+        " or ".html_safe +
+        (link_to "Register", new_user_registration_path, class: style)
+    end
+  end
 end

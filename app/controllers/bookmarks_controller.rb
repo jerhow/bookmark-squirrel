ce82d500cb227a77f07405179065ebc11c47b251
Jerry Howard <jerryhoward@fastmail.fm>
2019-08-10 04:29:23 +0000

    Implements Group auth/permission by User.id. Not wired up to anything yet.

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,30 @@
 class BookmarksController < ApplicationController
+  before_action :authorize
+  
   def index
   end
+
+  def show
+  end
+
+  private
+    def can_access?
+      group_id = params[:id]
+      users = Group.find_by(title: group_id).users
+      user_ids = []
+      
+      users.each do |u|
+        user_ids << u.id
+      end
+
+      user_ids.include? current_user.id
+    end
+
+    def authorize
+      if can_access?
+        puts "BOOSH"
+      else
+        puts "KAKOW"
+      end
+    end
 end

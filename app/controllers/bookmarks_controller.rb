1acc3ecd6018eb37d6fafe9d631d1bc8f1493493
Jerry Howard <jerryhoward@fastmail.fm>
2019-08-12 19:32:34 +0000

    Refactor of previous commit (95954bd) for clarity/separation of responsibilities

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,6 @@
 class BookmarksController < ApplicationController
-  before_action :authorize
+  before_action :setup
+  before_action :access_control
   
   def index
   end
@@ -14,27 +15,36 @@ class BookmarksController < ApplicationController
   end
 
   private
-    def can_access?
-      group_id = params[:id]
-      group = Group.find_by(title: group_id)
-      
-      if group.nil?
-        render :unknown, status: :not_found
-      else
-        users = group.users
-        user_ids = []
-        
-        users.each do |u|
-          user_ids << u.id
-        end
 
-        user_ids.include? current_user.id
+    def setup
+      @group_id = params[:id]
+      @group = Group.find_by(title: @group_id)
+    end
+
+    def access_control
+      if !group_exists? # 404
+        render :unknown, status: :not_found
+        return false
+      end
+
+      if !user_can_access_group? # 401
+        render :unauthorized, status: :unauthorized
+        return false
       end
     end
 
-    def authorize
-      if !can_access?
-        render :unauthorized, status: :unauthorized
+    def group_exists?
+      !@group.nil?
+    end
+
+    def user_can_access_group?
+      users = @group.users
+      user_ids = []
+      
+      users.each do |u|
+        user_ids << u.id
       end
+
+      user_ids.include? current_user.id
     end
 end

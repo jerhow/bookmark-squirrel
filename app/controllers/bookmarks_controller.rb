jerhow/bookmark-squirrel

commit 6116c45eefdbdd3a37a450548aa6db05dbf947c1

Fixes a bug in the 404 case of a group lookup on bookmarks#show

app/controllers/bookmarks_controller.rb
@@ -5,8 +5,7 @@ class BookmarksController < ApplicationController
   def show
     group_id = params[:id]
     @group = Group.find_by(id: group_id)
-    access_control(@group)
-    @bookmarks = @group.bookmarks.where(archived: false)
+    @bookmarks = @group.bookmarks.where(archived: false) if access_control(@group)
   end
 
   def new

commit 1b12954b9f80dfc4ee91b2c9903055946d8261d9

Fixes a bug that I introduced in commit 6116c45

app/controllers/bookmarks_controller.rb
@@ -99,6 +99,8 @@ class BookmarksController < ApplicationController
         render :unauthorized, status: :unauthorized # 401
         return false
       end
+
+      return true
     end
 
     def group_exists?(group)
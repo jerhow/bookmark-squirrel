235f423fe29b6f7e5a04c22409b383623a238308
Jerry Howard <jerryhoward@fastmail.fm>
2019-08-15 22:40:05 +0000

    Group updating now works on the group model (which is only the 'title' field at the moment).

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,4 +6,25 @@ class GroupsController < ApplicationController
     @group = Group.find_by(id: group_id)
     @users_in_group = @group.users.order(name: :asc) if access_control(@group)
   end
+
+  def update
+    group = Group.find_by(id: params[:id])
+    
+    respond_to do |format|
+      if group.update(group_params)
+        flash[:success] = "Group was successfully updated"
+        format.html { redirect_to action: "edit", id: group.id }
+      else
+        flash[:notice] = "Oops, there was a problem updating this group :/"
+        format.html { render :edit }
+      end
+    end
+  end
+
+  private
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def group_params
+      params.require(:group).permit(:title)
+    end
 end

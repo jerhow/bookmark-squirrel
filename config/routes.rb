c8bd2a0fd39375da684e51cb09686759d58a73fe
Jerry Howard <jerryhoward@fastmail.fm>
2019-08-17 06:32:01 +0000

    Implements adding users to groups

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'user/profile', to: 'users#show', as: 'show_user'
   resources :groups, path: 'g'
   delete 'g/:group_id/:user_id', to: 'groups#delete_user', as: 'delete_user_from_group'
+  get 'g/:group_id/new-user', to: 'groups#new_user', as: 'new_user_for_group'
+  post 'g/:group_id/new-user', to: 'groups#add_user', as: 'add_user_to_group'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

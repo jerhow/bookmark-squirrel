31bf707e8aea95fc066fff5badf3a5d9015f9f4e
Jerry Howard <jerryhoward@fastmail.fm>
2019-08-19 21:15:30 +0000

    Debugging Heroku, part 3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @group_count = current_user.groups.count
+    @group_count = current_user.groups.count if !current_user.nil?
   end
 end

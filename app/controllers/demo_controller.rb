00b57f99744e15796489091dbb0249c202585b0c
Jerry Howard <jerryhoward@fastmail.fm>
2019-08-23 21:47:13 +0000

    Upon creation of demo login credentials, they are stored in the session. We then redirect to the login form and populate the fields if these values exist in the session. Signing in with these demo accounts works as of now.

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,16 +1,27 @@
 class DemoController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:init]
+
   def init
     ts = Time.now.strftime '%Y%m%d%H%M%S%L'
     name = "demo_#{ts}"
+    email = "#{name}@bookmarksquirrel.com"
     pw = random_string(32)
-    user = User.new(name: name, email: "#{name}@bookmarksquirrel.com", password: pw)
+    user = User.new(name: name, email: email, password: pw)
     user.save!
+
+    session[:demo_email] = email
+    session[:demo_pw] = pw
+    forward_to_login(email, pw)
   end
 
   private
 
+    def forward_to_login(email, pw)
+      redirect_to new_user_session_path, email: email, pw: pw
+    end
+
     def random_string(n)
-      chars = Array('A'..'Z') + Array('a'..'z')
+      chars = Array('A'..'Z') + Array('a'..'z') + Array(0..9)
       Array.new(n) { chars.sample }.join
     end
 end

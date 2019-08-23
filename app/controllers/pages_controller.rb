4fb032bead7de9b987a2dcb2d43c24a33bab1fda
Jerry Howard <jerryhoward@fastmail.fm>
2019-08-23 20:29:56 +0000

    Require auth everywhere except on the home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:home]
+
   def home
     @group_count = current_user.groups.count if !current_user.nil?
   end

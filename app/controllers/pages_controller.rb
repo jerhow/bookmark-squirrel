4955eaf1206e009797719640a5f182313341306a
Jerry Howard <jerryhoward@fastmail.fm>
2019-08-26 21:23:40 +0000

    More updates to home page messaging + group totals by ownership and membership

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @group_count = current_user.groups.count if !current_user.nil?
+    if !current_user.nil?
+      groups = current_user.groups
+      @group_ownership_count = groups.where('owner_user_id = ?', current_user.id).count
+      @group_membership_count = groups.where('owner_user_id <> ?', current_user.id).count
+    end
   end
 end

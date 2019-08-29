40997a66211ed17747058eee0d8d66e178272324
Jerry Howard <jerryhoward@fastmail.fm>
2019-08-29 21:46:56 +0000

    Fixes the group users count lookup per group (n+1) query problem by looking up all the counts up front and storing them in a dictionary for fast lookups. Implemented in raw SQL - not sure I care to try bending ActiveRecord to this task.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def groups
     @groups = current_user.groups.order(created_at: :asc)
+    @group_user_counts = get_group_user_counts
   end
 
   def delete_avatar
@@ -16,4 +17,23 @@ class UsersController < ApplicationController
       format.html { redirect_to edit_user_registration_path }
     end
   end
+
+  private
+
+    def get_group_user_counts
+      sql = "SELECT gu.group_id, COUNT(gu.user_id) AS user_count " +
+          "FROM groups_users gu WHERE gu.group_id IN " +
+          "( SELECT group_id FROM groups_users WHERE user_id = #{current_user.id} ) " +
+          "GROUP BY gu.group_id ORDER BY gu.group_id"
+
+      counts = ActiveRecord::Base.connection.exec_query(sql)
+      group_user_counts = {}
+
+      counts.each do |c|
+        group_user_counts[ c['group_id'] ] = c['user_count']
+      end
+
+      group_user_counts
+    end
+
 end

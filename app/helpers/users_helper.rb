9c761db30ed07b80bbd7784cf8ce4e419403c251
Jerry Howard <jerryhoward@fastmail.fm>
2019-08-30 19:49:37 +0000

    Adds a bunch of styling to the 'view groups' page + member count and group ownership messaging

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,20 @@
 module UsersHelper
-  def manage_group_button(user, group)
-    if group.owner_user_id == current_user.id
+  def manage_group_button(group, user)
+    if group.owner_user_id == user.id
       link_to edit_group_path(group) do
         "<button id='btn_edit_bm_#{group.id}' 
           class='group_btn_edit' title='Manage this group'>Manage this group</button>".html_safe
       end
     else
       ''
+    end
+  end
+
+  def group_ownership_text(group, user)
+    if group.owner_user_id == user.id
+      "* You own this group"
+    else
+      ""
     end
   end
 end

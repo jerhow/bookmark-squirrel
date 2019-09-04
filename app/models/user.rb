20e9e83131dc88257deeb01a216f6b4ede914786
Jerry Howard <jerryhoward@fastmail.fm>
2019-09-04 22:19:34 +0000

    Layout/styling for the sign up page + warning message about signing up without the ability to recover one's password. Also disables the :recoverable module from Devise until we can get email set up.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+  # :confirmable, :lockable, :recoverable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable, :trackable
   has_and_belongs_to_many :groups
   has_one_attached :avatar
 

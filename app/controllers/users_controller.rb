2f6135cb394418539612745a4c12319d3afe23f5
Jerry Howard <jerryhoward@fastmail.fm>
2019-09-05 22:28:48 +0000

    Adds group bookmark counts to the groups summary page. Had to go around the ORM again for this one to avoid N+1 COUNT() queries for each group. This again prefetches the counts with a raw query and stores the results in a hash for fast lookups by group id.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def groups
     @groups = current_user.groups.order(created_at: :asc)
     @group_user_counts = get_group_user_counts
+    @group_bookmark_counts = get_group_bookmark_counts
   end
 
   def delete_avatar
@@ -21,10 +22,11 @@ class UsersController < ApplicationController
   private
 
     def get_group_user_counts
-      sql = "SELECT gu.group_id, COUNT(gu.user_id) AS user_count " +
-          "FROM groups_users gu WHERE gu.group_id IN " +
-          "( SELECT group_id FROM groups_users WHERE user_id = #{current_user.id} ) " +
-          "GROUP BY gu.group_id ORDER BY gu.group_id"
+      sql = "" +
+        "SELECT gu.group_id, COUNT(gu.user_id) AS user_count " +
+        "FROM groups_users gu WHERE gu.group_id IN " +
+        "( SELECT group_id FROM groups_users WHERE user_id = #{current_user.id} ) " +
+        "GROUP BY gu.group_id ORDER BY gu.group_id"
 
       counts = ActiveRecord::Base.connection.exec_query(sql)
       group_user_counts = {}
@@ -36,4 +38,23 @@ class UsersController < ApplicationController
       group_user_counts
     end
 
-end
+    def get_group_bookmark_counts
+      sql = "" +
+        "SELECT g.id AS group_id, COUNT(b.id) AS bookmark_count " +
+        "FROM groups g " +
+        "INNER JOIN groups_users gu ON g.id = gu.group_id " +
+        "INNER JOIN bookmarks b ON g.id = b.group_id " +
+        "WHERE gu.user_id = #{current_user.id} " +
+        "GROUP BY g.id " +
+        "ORDER BY g.id"
+
+        counts = ActiveRecord::Base.connection.exec_query(sql)
+        group_bookmark_counts = {}
+
+        counts.each do |c|
+          group_bookmark_counts[ c['group_id'] ] = c['bookmark_count']
+        end
+
+        group_bookmark_counts
+    end
+  end

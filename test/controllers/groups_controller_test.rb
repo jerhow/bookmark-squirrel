3ea976f15cae07f5e2616bf0de743956f30272f8
Jerry Howard <jerryhoward@fastmail.fm>
2019-09-11 23:14:09 +0000

    Adds tests for group ownership

--- test/controllers/groups_controller_test.rb
+++ test/controllers/groups_controller_test.rb
@@ -14,4 +14,18 @@ class GroupsControllerTest < ActionDispatch::IntegrationTest
     group.users << user
     assert user_can_access_group?(group, user)
   end
+
+  test "user owns group" do
+    group = Group.new(title: "", owner_user_id: 1)
+    user = User.new(id: 1, name: "", email: "")
+    group.users << user
+    assert is_group_owner?(group, user)
+  end
+
+  test "user does not own group" do
+    group = Group.new(title: "", owner_user_id: 1)
+    user = User.new(id: 2, name: "", email: "")
+    group.users << user
+    assert_not is_group_owner?(group, user)
+  end
 end

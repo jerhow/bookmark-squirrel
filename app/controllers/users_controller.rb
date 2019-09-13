ac954a35ff8c2ff223e6aa7966984bf7b9b08730
Jerry Howard <jerryhoward@fastmail.fm>
2019-09-13 20:21:53 +0000

    Moves 'get_group_user_counts' and 'get_group_bookmark_counts' from the Users controller to the Group model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,8 @@ class UsersController < ApplicationController
 
   def groups
     @groups = current_user.groups.order(created_at: :asc)
-    @group_user_counts = get_group_user_counts
-    @group_bookmark_counts = get_group_bookmark_counts
+    @group_user_counts = Group.get_group_user_counts(current_user)
+    @group_bookmark_counts = Group.get_group_bookmark_counts(current_user)
   end
 
   def delete_avatar
@@ -18,43 +18,4 @@ class UsersController < ApplicationController
       format.html { redirect_to edit_user_registration_path }
     end
   end
-
-  private
-
-    def get_group_user_counts
-      sql = "" +
-        "SELECT gu.group_id, COUNT(gu.user_id) AS user_count " +
-        "FROM groups_users gu WHERE gu.group_id IN " +
-        "( SELECT group_id FROM groups_users WHERE user_id = #{current_user.id} ) " +
-        "GROUP BY gu.group_id ORDER BY gu.group_id"
-
-      counts = ActiveRecord::Base.connection.exec_query(sql)
-      group_user_counts = {}
-
-      counts.each do |c|
-        group_user_counts[ c['group_id'] ] = c['user_count']
-      end
-
-      group_user_counts
-    end
-
-    def get_group_bookmark_counts
-      sql = "" +
-        "SELECT g.id AS group_id, COUNT(b.id) AS bookmark_count " +
-        "FROM groups g " +
-        "INNER JOIN groups_users gu ON g.id = gu.group_id " +
-        "INNER JOIN bookmarks b ON g.id = b.group_id " +
-        "WHERE gu.user_id = #{current_user.id} " +
-        "GROUP BY g.id " +
-        "ORDER BY g.id"
-
-        counts = ActiveRecord::Base.connection.exec_query(sql)
-        group_bookmark_counts = {}
-
-        counts.each do |c|
-          group_bookmark_counts[ c['group_id'] ] = c['bookmark_count']
-        end
-
-        group_bookmark_counts
-    end
-  end
+end
--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,43 @@ class Group < ApplicationRecord
   has_many :bookmarks
 
   validates_presence_of :title
+
+  private
+
+    def self.get_group_user_counts(user)
+      sql = "" +
+        "SELECT gu.group_id, COUNT(gu.user_id) AS user_count " +
+        "FROM groups_users gu WHERE gu.group_id IN " +
+        "( SELECT group_id FROM groups_users WHERE user_id = #{user.id} ) " +
+        "GROUP BY gu.group_id ORDER BY gu.group_id"
+
+      counts = ActiveRecord::Base.connection.exec_query(sql)
+      group_user_counts = {}
+
+      counts.each do |c|
+        group_user_counts[ c['group_id'] ] = c['user_count']
+      end
+
+      group_user_counts
+    end
+
+    def self.get_group_bookmark_counts(user)
+      sql = "" +
+        "SELECT g.id AS group_id, COUNT(b.id) AS bookmark_count " +
+        "FROM groups g " +
+        "INNER JOIN groups_users gu ON g.id = gu.group_id " +
+        "INNER JOIN bookmarks b ON g.id = b.group_id " +
+        "WHERE gu.user_id = #{user.id} " +
+        "GROUP BY g.id " +
+        "ORDER BY g.id"
+
+        counts = ActiveRecord::Base.connection.exec_query(sql)
+        group_bookmark_counts = {}
+
+        counts.each do |c|
+          group_bookmark_counts[ c['group_id'] ] = c['bookmark_count']
+        end
+
+        group_bookmark_counts
+    end
 end

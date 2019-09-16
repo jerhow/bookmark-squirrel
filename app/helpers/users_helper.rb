4d3e08a40238c8e1d74edd7dfed770242160c7d5
Jerry Howard <jerryhoward@fastmail.fm>
2019-09-16 20:52:43 +0000

    Replaces 'Create a new group' link with button on groups page + updates style of 'Manage this group' buttons on same page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
     if group.owner_user_id == user.id
       link_to edit_group_path(group) do
         "<button id='btn_edit_bm_#{group.id}' 
-          class='group_btn_edit' title='Manage this group'>Manage this group</button>".html_safe
+          class='btn_blue small' title='Manage this group'>Manage this group</button>".html_safe
       end
     else
       ''
